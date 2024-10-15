0131701ed73b08bf0532071fc2b728de3eaf2fc0
KalpanaKosmik <kalpanareddy937@gmail.com>
2024-10-15 13:47:49 +0530

    I have added Additional Test case

---
+++ TestNG_Practise/src/Dropdown/New_Sample.java
@@ -0,0 +1,11 @@
+package Dropdown;
+
+public class New_Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Updated new class");
+
+	}
+
+}
